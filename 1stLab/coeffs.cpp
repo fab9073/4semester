6639f7f0fd4b90e41b76b1f459895ac219ecd8b1
artemocky <72733482+artemocky@users.noreply.github.com>
2021-05-18 21:15:47 +0300

    Update coeffs.cpp

--- 1stLab/coeffs.cpp
+++ 1stLab/coeffs.cpp
@@ -4,19 +4,19 @@ Coeffs::Coeffs() {}
 Coeffs::Coeffs(int count) { countOfCoeffs = count; coeff = new double[count]; }
 
 void Coeffs::setCoeffs() {
-	std::cout << "Please, enter " << countOfCoeffs << " coefficents." << std::endl;
-	std::cout << "First coefficient refers to the highest degree of the polynomial." << std::endl;
+	cout << "Please, enter " << countOfCoeffs << " coefficents." << endl;
+	cout << "First coefficient refers to the highest degree of the polynomial." << endl;
 	for (int i = 0; i < countOfCoeffs; i++) {
-		std::cout << "Enter " << i + 1 << " coefficient: ";
-		std::cin >> coeff[i];
+		cout << "Enter " << i + 1 << " coefficient: ";
+		cin >> coeff[i];
 	}
-	std::cout << std::endl;
+	cout << endl;
 }
 
 void Coeffs::printCoeffs() {
-	std::cout << "Entered coefficents:" << std::endl;
+	cout << "Entered coefficents:" << endl;
 	for (int i = 0; i < countOfCoeffs; i++) {
-		std::cout << coeff[i] << std::endl;
+		cout << coeff[i] << endl;
 	}
 }
 
